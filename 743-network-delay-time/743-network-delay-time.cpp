1a90d9a2d77e638b845621cc12f1717efef0e3d8
rajesh2k20 <60920805+rajesh2k20@users.noreply.github.com>
2022-04-30 11:59:14 +0530

    Time: 272 ms (15.35%), Space: 43.8 MB (23.99%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,26 +1,48 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-      
-       vector<int> dp(n+1,1e9);
-       dp[k]=0;
-       for(int i=1; i<n;i++){
-          
-           for(auto e : times ){
-               int u= e[0];
-               int v=e[1];
-               int w=e[2];
-               dp[v]= min(dp[v], dp[u]+w);
-           }
-       }
-      int ans=0;
-     for(int i=1; i<=n;i++){
-         
-         if(dp[i]>=1e9){
-             return -1;
-         }
-         ans=max(dp[i], ans);
-     }
-     return ans;
+         vector<vector<pair<int, int>>> adj(n+1);
+        
+          for(auto  e : times){
+              
+              int u= e[0];
+              int v= e[1];
+              int w= e[2];
+              
+              adj[u].push_back({v,w});
+          }
+        vector<int> dp(n+1, 1e9);
+        dp[k]=0;
+        
+        set<pair<int, int>> s;
+        s.insert({0,k});
+        while(s.size()>0){
+            
+         pair<int, int> curr= *(s.begin());
+         int u= curr.second;
+         s.erase(s.begin());
+            
+         for( auto  nb : adj[u]){
+             
+             int v= nb.first;
+             int w= nb.second;
+             
+             if(dp[v]> dp[u]+w){
+                 s.erase({dp[v], v});
+                 dp[v]=dp[u]+w;
+                 s.insert({dp[v], v});
+             }
+         }    
+            
+        }
+        int ans=0;
+        for(int i=1; i<=n;i++){
+            
+            if(dp[i]>=1e9){
+                return -1;
+            }
+            ans= max(dp[i], ans);
+        }
+        return ans;
     }
 };
