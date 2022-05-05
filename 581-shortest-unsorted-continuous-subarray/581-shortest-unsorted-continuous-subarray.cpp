66dfdece61d4d78a0ab02fded1fd84bdad832e54
rajesh2k20 <60920805+rajesh2k20@users.noreply.github.com>
2022-05-05 10:49:20 +0530

    Time: 43 ms (59.17%), Space: 27.3 MB (48.68%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -23,7 +23,7 @@ public:
               break;
           }
       }
-       cout<<index1<<" "<<index2<<endl; 
+       // cout<<index1<<" "<<index2<<endl; 
       return index2-index1+1;
       
      
