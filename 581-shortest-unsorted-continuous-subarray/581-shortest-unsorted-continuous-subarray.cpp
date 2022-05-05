c3f6f018db568872097b00386411df56c4c6b6ab
rajesh2k20 <60920805+rajesh2k20@users.noreply.github.com>
2022-05-05 11:18:10 +0530

    Time: 48 ms (43.33%), Space: 28.1 MB (9.74%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,31 +1,79 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
+      int n=nums.size();
+     vector<int> suffixmin(n);
+      suffixmin[n-1]=nums[n-1];
+     for(int i=n-2; i>=0;i--){
+       
+        suffixmin[i]= min(suffixmin[i+1], nums[i]);
+     }
+     int index1=-1;
      
-     vector<int> arr(nums.begin(), nums.end());
-      sort(arr.begin(), arr.end());
-      int index1=-1;
-      int index2=-1;
-      if(arr==nums){
-          return 0;
-      }
-      for(int i=0; i<nums.size();i++){
+        
+      for(int i=0; i<n;i++){
           
-          if(arr[i] != nums[i]){
+          if(i==0){
+              
+              if(suffixmin[i]!=nums[i]){
+                  index1=i;
+                  break;
+              }
+              continue;
+          }
+          
+          if(nums[i-1]<=nums[i] ){
+              
+             if(suffixmin[i]!=nums[i]){
+                 index1=i;
+                 break;
+             }
+          }else{
               index1=i;
-              break;
+              break ;
           }
       }
-      for(int i=nums.size()-1 ;i>=0; i--){
-          
-          if(arr[i]!=nums[i]){
-              index2=i;
-              break;
-          }
-      }
-       // cout<<index1<<" "<<index2<<endl; 
-      return index2-index1+1;
-      
+     if(index1==-1){
+         return 0;
+     }
+     int index2=-1;
+     vector<int> prefixmax(n);
+     prefixmax[0]=nums[0];
+     for(int i=1; i<n;i++){
+         
+         prefixmax[i]= max(prefixmax[i-1], nums[i]);
+     }
      
+     for(int i=n-1; i>=0; i--){
+         
+         if(i==n-1){
+            
+            if(prefixmax[i] != nums[i]){
+                index2=i;
+                break;
+            }
+            continue;
+             
+         }
+         
+         if(nums[i]<=nums[i+1]){
+             
+             if(prefixmax[i] != nums[i]){
+                index2=i;
+                break;
+            }
+              
+         }else{
+             
+             index2=i;
+             break;
+         }
+     }
+      cout<<index1<<" "<<index2<<endl; 
+     return index2-index1+1;
+        
+        
+        
+        
     }
 };
