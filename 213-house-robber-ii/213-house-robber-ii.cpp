b117ce10c2ceb4d871d0c2e2d894ce1a1e5b5bc1
rajesh2k20 <60920805+rajesh2k20@users.noreply.github.com>
2022-07-21 12:04:04 +0530

    Time: 3 ms (58.27%), Space: 7.8 MB (61.89%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,63 +1,50 @@
+int dp[101][101];
 class Solution {
 public:
-    int helper(vector<int>  arr){
+    
+    int helper(int index, int endindex, int n, vector<int> & arr){
         
-     
-        int n=arr.size();
-        
-           if(n==0){
+        if(n==0 || index>endindex){
+            
             return 0;
         }
         
-        int dp[n];
-        memset(dp, 0 , sizeof(dp));
-        
-        for(int i=0; i<n;i++){
+        if(dp[index][n] !=-1){
             
-            if(i==0){
-                
-                dp[i]=arr[i];
-                continue;
-            }
-            
-            if(i==1){
-                dp[i]=max(arr[i], arr[i-1]);
-                
-                continue;
-            }
-            
-            dp[i]= max(dp[i-1], dp[i-2]+arr[i]);
+            return dp[index][n];
         }
         
-        return dp[n-1];
+        int taken= arr[index]+helper(index+2, endindex, n-1, arr);
+        int notTaken= helper(index+1, endindex, n, arr);
+        
+        return dp[index][n]=max(taken, notTaken);
     }
     int rob(vector<int>& nums) {
         
-        if(nums.size()==1){
-            
-            return nums[0];
-        }
+      
+     int n= nums.size();
         
-        if(nums.size()==2){
-            
-            return max(nums[0], nums[1]);
-        }
-        int n=nums.size();
-        vector<int> arr1;
-        vector<int> arr2;
-        
-        for(int i=1; i<n;i++){
-            
-           arr1.push_back(nums[i]); 
-        }
-        
-        for(int i=2; i<n-1;i++){
-            arr2.push_back(nums[i]);
-        }
-        cout<<arr1.size()<<" "<<arr2.size()<<endl;
-        int ans1= helper(arr1);
-        int ans2= nums[0]+helper(arr2);
-        return max(ans1, ans2);
+      if(n==1){
+          return nums[0];
+      }
+     if(n%2==0){
+         memset(dp, -1, sizeof(dp));
+         int ans1= helper(0, n-2, n/2, nums);
+          memset(dp, -1, sizeof(dp));
+         int ans2= helper(1, n-1, n/2, nums);
+         return max(ans1, ans2);
+     }else{
+          memset(dp, -1, sizeof(dp));
+         int ans1= helper(0, n-2,n/2, nums);
+          memset(dp, -1, sizeof(dp));
+         int ans2= helper(1, n-1, n/2, nums);
+          memset(dp, -1, sizeof(dp));
+         int ans3= helper(0, n-2, (n/2)+1, nums);
+          memset(dp, -1, sizeof(dp));
+         int ans4= helper(1, n-1, (n/2)+1, nums);
+         return max({ans1, ans2, ans3, ans4});
+     }
+     
         
     }
 };
