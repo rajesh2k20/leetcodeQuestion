f83bd0ed5eb2ff3752e372fda416920e76044c3d
rajesh2k20 <60920805+rajesh2k20@users.noreply.github.com>
2022-08-16 15:38:13 +0530

    Time: 576 ms (43.68%), Space: 27.4 MB (29.25%) - LeetHub

---
+++ 1986-minimum-number-of-work-sessions-to-finish-the-tasks/1986-minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -0,0 +1,57 @@
+int dp[1<<15][150];
+class Solution {
+public:
+    int helper(int mask, vector<int> & arr, int st, int sum){
+        
+        if(mask==0){
+            
+            return 1;
+        }
+        
+        if(dp[mask][sum] !=-1){
+            
+            return dp[mask][sum];
+        }
+        
+        
+        int ans=1e9;
+        for(int i=0; i<arr.size();i++){
+            
+            
+            int bit=  (mask>>i ) & 1;
+            
+            if(bit==1){
+                
+                if(sum+ arr[i]<=st){
+                    
+                 ans= min(ans, helper(mask ^ (1<<i), arr, st, sum+arr[i]));
+                  
+                }else{
+                    
+                    ans= min(ans, 1+helper(mask ^ (1<<i), arr, st, arr[i]));
+                }
+            }
+            
+           
+             
+                
+         }
+           return dp[mask][sum]=ans;
+        
+    }
+    int minSessions(vector<int>& tasks, int sessionTime) {
+        
+        int n=tasks.size();
+        
+        int mask= 1<<(n);
+        mask--;
+        
+        cout<<mask<<endl;
+        
+        memset(dp, -1, sizeof(dp));
+        
+        return helper(mask, tasks, sessionTime, 0);
+        
+        
+    }
+};
